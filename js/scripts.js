0eda3fb89d11a82cae3b8ab6a42bf55c7e0234c1
Kyle Legare <kylelegare@gmail.com>
2015-07-29 20:47:35 -0700

    Initial push

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// $('body').on('click', function () {
+// $('body').css({
+//  'backgroundColor': 'red'
+//  });
+// });
+
+
+// $('.test-class').on('click', function() {
+// 	$('.test-class').css({
+// 		'fontSize': '4em'
+// 	});
+// });
+
+// $('body').on('kyleTestEvent'), function() {
+// 		alert('test');
+// });
+
+// $('img').on('click', function () {
+// 	$('body').trigger('kyleTestEvent');
+// });
+
+
+$('.img-circle').on('show.bs.modal', function () {
+	$(.'big-image').attr('src', 'http://cache4.asset-cache.net/xc/532415641.jpg?v=2&c=IWSAsset&k=2&d=dqxR9dcuALWOuHNfqpwCHYGYxvoGYgRZvs_t7uqNR1etm6Ciy-SdjvYgXD2-kTMm0')
+});
