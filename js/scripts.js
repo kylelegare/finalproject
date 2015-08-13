c417f9c182903e65f6ebcb1400cd93ea72ff6309
Kyle Legare <kylelegare@gmail.com>
2015-08-12 19:05:46 -0700

    august 12th class

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,13 @@
-// $('body').on('click', function () {
-// $('body').css({
-//  'backgroundColor': 'red'
-//  });
-// });
+// Options
+var options = {
+	offset: "#display-header",
+	offsetSide: "top",
+	classes: {
+		clone: 'banner--clone',
+		stick: 'banner--stick',
+		unstick: 'banner--unstick'
+	}
 
+}
 
-// $('.test-class').on('click', function() {
-// 	$('.test-class').css({
-// 		'fontSize': '4em'
-// 	});
-// });
-
-// $('body').on('kyleTestEvent'), function() {
-// 		alert('test');
-// });
-
-// $('img').on('click', function () {
-// 	$('body').trigger('kyleTestEvent');
-// });
-
-
-$('.img-circle').on('show.bs.modal', function () {
-	$(.'big-image').attr('src', 'http://cache4.asset-cache.net/xc/532415641.jpg?v=2&c=IWSAsset&k=2&d=dqxR9dcuALWOuHNfqpwCHYGYxvoGYgRZvs_t7uqNR1etm6Ciy-SdjvYgXD2-kTMm0')
-});
+var header = new Headhesive('header', options);
